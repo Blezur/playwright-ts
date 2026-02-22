ad7f99eb3c8ddcfda91c39c545fa82734ac1a45a
Blezur <bapgoj@gmail.com>
2026-02-22 14:09:49 +0100

    Add error handling tests for login functionality

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,63 @@
 import { TEST_USERS } from '../src/config';
 import { test, expect } from '../src/fixtures/login.fixture';
 
-test('should login as standard user and land on inventory page', async ({
-  page,
-  loginAs,
-}) => {
-  await loginAs(TEST_USERS.standard_user);
-  await expect(page.getByTestId('inventory-container')).toBeVisible();
+test.describe('Login Tests', () => {
+  test('should login as standard user and land on inventory page', async ({
+    loginAs,
+    page,
+  }) => {
+    await loginAs(TEST_USERS.standard_user);
+    await expect(page.getByTestId('inventory-container')).toBeVisible();
+  });
+
+  test('should show error message when logging in with locked out user', async ({
+    loginPage,
+  }) => {
+    await loginPage.login(TEST_USERS.locked_out_user);
+    await loginPage.expectErrorState(
+      'Epic sadface: Sorry, this user has been locked out.',
+    );
+  });
+
+  test('should show error message when logging in with invalid credentials', async ({
+    loginPage,
+  }) => {
+    await loginPage.login({
+      username: 'invalid_user',
+      password: 'invalid_pass',
+    });
+    await loginPage.expectErrorState(
+      'Epic sadface: Username and password do not match any user in this service',
+    );
+  });
+
+  test('should show error message when username field is empty', async ({
+    loginPage,
+  }) => {
+    await loginPage.login({
+      username: '',
+      password: 'secret_sauce',
+    });
+    await loginPage.expectErrorState('Epic sadface: Username is required');
+  });
+
+  test('should show error message when username and password fields are empty', async ({
+    loginPage,
+  }) => {
+    await loginPage.login({
+      username: '',
+      password: '',
+    });
+    await loginPage.expectErrorState('Epic sadface: Username is required');
+  });
+
+  test('should show error message when password field is empty', async ({
+    loginPage,
+  }) => {
+    await loginPage.login({
+      username: 'standard_user',
+      password: '',
+    });
+    await loginPage.expectErrorState('Epic sadface: Password is required');
+  });
 });
